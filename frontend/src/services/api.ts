5660ea2f887e6273b1fb16d8bacb1d499d063a6f
Kevin Santamaria <kevinsdasere@gmail.com>
2026-02-06 11:12:30 -0500

    fix: fix wrong code

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,13 +1,6 @@
 import axios, { AxiosError } from "axios";
 
-<<<<<<< HEAD
 const URL = import.meta.env.VITE_API_URL;
-
-console.log(URL);
-=======
-const API_URL = "http://localhost:3000/api";
->>>>>>> 5795303c9a4d90118ac020e5cb21a9fd84d2522e
-
 const api = axios.create({
   baseURL: URL,
   headers: {
