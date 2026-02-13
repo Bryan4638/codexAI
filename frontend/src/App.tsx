8c61618f310e0c86b7cf7462ea0da98946d6c5de
Kevin Santamaria <kevinsdasere@gmail.com>
2026-02-13 02:06:50 -0500

    feat: add TanStack Query to the project

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,15 @@
 import AppRouter from '@/routes/AppRouter'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 function App() {
-  return <AppRouter />
+  const queryClient = new QueryClient()
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AppRouter />
+      {/* <ReactQueryDevtools /> */}
+    </QueryClientProvider>
+  )
 }
 
 export default App
