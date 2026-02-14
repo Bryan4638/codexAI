4ae5711d6bf1d66a564339441c7c7530d3eec53c
Kevin Santamaria <kevinsdasere@gmail.com>
2026-02-13 19:44:40 -0500

    feat: create component for required login view

---
+++ frontend/src/components/share/LoginRequired.tsx
@@ -0,0 +1,25 @@
+import AuthModal from '@/components/auth/AuthModal'
+import { useState } from 'react'
+
+export default function LoginRequired() {
+  const [showAuthModal, setShowAuthModal] = useState<boolean>(false)
+
+  return (
+    <>
+      <section className="pt-32 max-w-7xl mx-auto px-6 text-center">
+        <h2 className="text-3xl">Acceso Restringido</h2>
+        <p className="mb-10 mt-2 text-sm">
+          Inicia sesión para desbloquear medallas.
+        </p>
+
+        <button
+          onClick={() => setShowAuthModal(true)}
+          className="btn btn-primary"
+        >
+          Iniciar sesión
+        </button>
+      </section>
+      {showAuthModal && <AuthModal onClose={() => setShowAuthModal(false)} />}
+    </>
+  )
+}
