4f8d2ca444c3f5ab9d130b22562d5d07933f0f15
Kevin Santamaria <kevinsdasere@gmail.com>
2026-02-28 21:14:27 -0500

    feat: se crea el directorio 'styles' que contiene el archivo principal de estilos y el rchivo de fuentes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,7 +10,6 @@ function App() {
       <AuthInitializer>
         <AppRouter />
       </AuthInitializer>
-      {/* <ReactQueryDevtools /> */}
     </QueryClientProvider>
   )
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,5 @@
 import App from '@/App'
-import '@/index.css'
+import '@/styles/index.css'
 import { createRoot } from 'react-dom/client'
 
 createRoot(document.getElementById('root')!).render(<App />)
